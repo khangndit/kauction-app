0fe805a4317c3c526ba2d7ec5f809cc2f285de73
“khangndit” <khangndit@gmail.com>
2021-09-10 14:40:44 +0700

    initial commit

--- src/shared/socket/socket.js
+++ src/shared/socket/socket.js
@@ -1,5 +1,7 @@
 import { createContext } from 'react';
 import io from 'socket.io-client';
 
-export const socket = io.connect('http://localhost:8080');
+// export const socket = io.connect('http://localhost:8080');
+export const socket = io.connect('https://kauction-server.herokuapp.com/');
+
 export const SocketContext = createContext(socket);
