d442c197763f07a6589c6bbb1e91bc07f84cda86
“khangndit” <khangndit@gmail.com>
2021-09-10 15:32:12 +0700

    fix url home

--- src/components/MyLogo/Logo.jsx
+++ src/components/MyLogo/Logo.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 const Logo = () => {
   return (
-    <Link to="/">
+    <Link to="/kauction-app">
       <div className="logo__wrapper">
         <img
           className="logo-img pointer"
